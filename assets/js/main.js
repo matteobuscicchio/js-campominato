3107e067707d2616686c530d9f279bc70d217cdd
matteo <matteoezio@me.com>
2020-11-24 18:03:58 +0100

    update 2 esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,3 @@
-// Descrizione
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 =>  tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
-// Consigli del giorno:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Proviamo prima con pochi numeri, inserire 86 numeri ogni volta potrebbe essere un po’ scocciante :wink:
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
-// Ricordatevi che se non sappiamo quante volte dobbiamo fare una cosa ci serve.
-
-
-
-
-// SVOGLIMENTO
 // checker
 function checker(numOne, numTwo) {
     var index = 0;
@@ -34,7 +8,7 @@ function checker(numOne, numTwo) {
         index++;
     }
 }
-
+// scelta difficoltà
 function difficultyParameter() {
     difficultyArray = [];
     var difficultyArray = Number(prompt('scegli la difficoltà; digita 0 per scegliere facile, 1 per medio e 2 per difficile'));
@@ -62,7 +36,6 @@ function difficultyParameter() {
     }
     return difficulty;
 }
-
 // numeri random
 function bombs() {
     var numberArray = [];
@@ -76,7 +49,6 @@ function bombs() {
     // console.log(oderedCpNumber);
     return oderedCpNumber;
 }
-
 // scelta numero utente
 function userLuck() {
     userNumberArray = [];
@@ -100,7 +72,7 @@ function userLuck() {
         alert('Complimenti, hai vinto. Hai dato '+ userNumberArray.length + ' risposte esatte consecutivamente. Il tuo puntegio finale è ' + bomb);
     }
 }
-
+//ESECUZIONE
 difficultyParameter();
 bombs();
 userLuck();
