3425719093fac36fa5ddc2b3120b11138e9fc82d
matteo <matteoezio@me.com>
2020-11-24 18:00:30 +0100

    completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,33 +35,57 @@ function checker(numOne, numTwo) {
     }
 }
 
+function difficultyParameter() {
+    difficultyArray = [];
+    var difficultyArray = Number(prompt('scegli la difficoltà; digita 0 per scegliere facile, 1 per medio e 2 per difficile'));
+    var index=0;
+    while (index < 1) {
+        if (difficultyArray == 0) {
+            bomb = 100;
+            difficulty = 84;
+            console.log(difficulty);
+            break;
+        }
+        if (difficultyArray == 1) {
+            bomb = 80;
+            difficulty = 64;
+            console.log(difficulty);
+            break;
+        }
+        if (difficultyArray == 2) {
+            bomb = 50;
+            difficulty = 34;
+            console.log(difficulty);
+            break;
+        }
+        index++;
+    }
+    return difficulty;
+}
+
 // numeri random
-function cpRandomNumber() {
+function bombs() {
     var numberArray = [];
-    oderedCpNumber = numberArray.sort();
     while(numberArray.length < 16){
-        var randomNumber = Math.floor(Math.random() * 20) + 1; //sostituisci  20 con 100
+        var randomNumber = Math.floor(Math.random() * 100) + 1;
         if (numberArray.indexOf(randomNumber) === -1) {
             numberArray.push(randomNumber)
         }
     }
-    console.log(oderedCpNumber.sort());
+    oderedCpNumber = numberArray.sort();
+    console.log(oderedCpNumber);
     return oderedCpNumber;
 }
 
 // scelta numero utente
 function userLuck() {
     userNumberArray = [];
-    while(userNumberArray.length < 1) {
+    while(userNumberArray.length < difficulty ) {
         userNumber = Number(prompt('inserisci un numero'));
         if (checker(oderedCpNumber, userNumber)) {
-            console.log('spiacente hai perso');
+            alert('Spiacente, hai perso. Con un punteggio pari a ' + userNumberArray.length);
             break;
         }
-        // if (checker(userNumberArray, userNumber)) {
-        //     console.log('hai vinto');
-        //     break;
-        // }
         if (userNumber === 0) {
             alert('il numero zero non è ammesso, scegline uno nuovo')
         } else if (userNumber > 100) {
@@ -72,10 +96,11 @@ function userLuck() {
             userNumberArray.push(userNumber);
         }
     }
-    orderedUserNumber = userNumberArray.sort();
-    console.log(orderedUserNumber);
+    if (userNumberArray.length == difficulty) {
+        alert('Complimenti, hai vinto. Hai dato '+ difficulty + ' risposte esatte consecutivamente. Il tuo puntegio finale è ' + bomb);
+    }
 }
 
-cpRandomNumber();
+difficultyParameter();
+bombs();
 userLuck();
-checker(userNumberArray, userNumber);
