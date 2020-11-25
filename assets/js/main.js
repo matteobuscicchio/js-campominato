c801be78df1723b2570f4f083bf2a0b89a14ecef
matteo <matteoezio@me.com>
2020-11-25 10:44:27 +0100

    refactoring in corso

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,70 +9,66 @@ function checker(numOne, numTwo) {
     }
 }
 // scelta difficoltà
-function difficultyParameter() {
-    difficultyArray = [];
-    var difficultyArray = Number(prompt('scegli la difficoltà; digita 0 per scegliere facile, 1 per medio e 2 per difficile'));
-    var index=0;
-    while (index < 1) {
-        if (difficultyArray == 0) {
-            bomb = 100;
-            difficulty = 84;
-            console.log(difficulty);
-            break;
-        }
-        if (difficultyArray == 1) {
-            bomb = 80;
-            difficulty = 64;
-            console.log(difficulty);
-            break;
-        }
-        if (difficultyArray == 2) {
-            bomb = 50;
-            difficulty = 34;
-            console.log(difficulty);
-            break;
-        }
-        index++;
+var bomb, difficulty;
+var sceltaLivello = Number(prompt('scegli la difficoltà; digita 0 per scegliere facile, 1 per medio e 2 per difficile', '0, 1 ,2'));
+var index=0;
+while (index < 1) {
+    if (sceltaLivello == 0) {
+        bomb = 100;
+        difficulty = 84;
+        console.log(difficulty);
+        break;
     }
-    return difficulty;
+    if (sceltaLivello == 1) {
+        bomb = 80;
+        difficulty = 64;
+        console.log(difficulty);
+        break;
+    }
+    if (sceltaLivello == 2) {
+        bomb = 50;
+        difficulty = 34;
+        console.log(difficulty);
+        break;
+    }
+    index++;
 }
 // numeri random
-function bombs() {
+function bombs(gameBomb) {
     var numberArray = [];
     while(numberArray.length < 16){
-        var randomNumber = Math.floor(Math.random() * 100) + 1;
+        var randomNumber = Math.floor(Math.random() * gameBomb) + 1;
         if (numberArray.indexOf(randomNumber) === -1) {
             numberArray.push(randomNumber)
         }
     }
     oderedCpNumber = numberArray.sort();
-    // console.log(oderedCpNumber);
+    console.log(oderedCpNumber);
     return oderedCpNumber;
 }
 // scelta numero utente
-function userLuck() {
+function userLuck(gameBomb, gameDifficulty) {
     userNumberArray = [];
-    while(userNumberArray.length < difficulty ) {
+    while(userNumberArray.length < gameDifficulty ) {
         userNumber = Number(prompt('inserisci un numero'));
         if (checker(oderedCpNumber, userNumber)) {
             alert('Spiacente, hai perso. Con un punteggio pari a ' + userNumberArray.length);
             break;
         }
         if (userNumber === 0) {
-            alert('il numero zero non è ammesso, scegline uno nuovo')
-        } else if (userNumber > 100) {
-            alert('questo numero non è ammesso, scegline uno nuovo')
+            alert('il numero zero non è ammesso, scegline uno nuovo');
+        } else if (userNumber > gameBomb) {
+            alert('questo numero non è ammesso, scegline uno nuovo');
         } else if (userNumberArray.indexOf(userNumber) !== -1) {
-            alert('hai già inserito questo numero, scegline uno nuovo')
+            alert('hai già inserito questo numero, scegline uno nuovo');
         } else {
             userNumberArray.push(userNumber);
         }
     }
-    if (userNumberArray.length == difficulty) {
-        alert('Complimenti, hai vinto. Hai dato '+ userNumberArray.length + ' risposte esatte consecutivamente. Il tuo puntegio finale è ' + bomb);
+    if (userNumberArray.length == gameDifficulty) {
+        alert('Complimenti, hai vinto. Hai dato '+ userNumberArray.length + ' risposte esatte consecutivamente. Il tuo puntegio finale è ' + gameBomb);
     }
 }
 //ESECUZIONE
-difficultyParameter();
-bombs();
-userLuck();
+bombs(bomb);
+userLuck(bomb, difficulty);
